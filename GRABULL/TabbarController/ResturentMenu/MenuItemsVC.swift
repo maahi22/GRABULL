cfc1c9a8c66ee70bfdc7d6ea834ab280679750b2
Mahendra <mahendrasingh9211@gmail.com>
2018-03-15 23:58:09 +0530

    changes
    changes

--- GRABULL/TabbarController/ResturentMenu/MenuItemsVC.swift
+++ GRABULL/TabbarController/ResturentMenu/MenuItemsVC.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class MenuItemsVC: UIViewController {
 
+    
+    let kHeaderSectionTag: Int = 6900;
+    var expandedSectionHeaderNumber: Int = -1
+    var expandedSectionHeader: UITableViewHeaderFooterView!
+    
+    var sectionItems: Array<Any> = []
+    var sectionNames: Array<Any> = []
+    
+    
+    @IBOutlet weak var menuTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        
+        sectionNames = [ "iPhone", "iPad", "Apple Watch" ];
+        sectionItems = [ ["iPhone 5", "iPhone 5s", "iPhone 6", "iPhone 6 Plus", "iPhone 7", "iPhone 7 Plus"],
+                         ["iPad Mini", "iPad Air 2", "iPad Pro", "iPad Pro 9.7"],
+                         ["Apple Watch", "Apple Watch 2", "Apple Watch 2 (Nike)"]
+        ];
+        self.menuTableView!.tableFooterView = UIView()
         
        /* self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
@@ -57,15 +73,130 @@ private let reuseCartCell = "CartCell"
 
 extension MenuItemsVC:UITableViewDelegate,UITableViewDataSource{
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        
-        return 2.0
-        
-    }
+    
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        if sectionNames.count > 0 {
+            tableView.backgroundView = nil
+            return sectionNames.count
+        } else {
+            let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: view.bounds.size.height))
+            messageLabel.text = "Retrieving data.\nPlease wait."
+            messageLabel.numberOfLines = 0;
+            messageLabel.textAlignment = .center;
+            messageLabel.font = UIFont(name: "HelveticaNeue", size: 20.0)!
+            messageLabel.sizeToFit()
+            self.menuTableView.backgroundView = messageLabel;
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (self.sectionNames.count != 0) {
+            return "Chilli potato"//self.sectionNames[section] as? String
+        }
+        return ""
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44.0;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat{
+        return 0;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        //recast your view as a UITableViewHeaderFooterView
+        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+        header.contentView.backgroundColor = UIColor.hexStringToUIColor(hex: "#408000")
+        header.textLabel?.textColor = UIColor.white
+        
+        if let viewWithTag = self.view.viewWithTag(kHeaderSectionTag + section) {
+            viewWithTag.removeFromSuperview()
+        }
+        let headerFrame = self.view.frame.size
+        let theImageView = UIImageView(frame: CGRect(x: headerFrame.width - 32, y: 13, width: 18, height: 18));
+        theImageView.image = UIImage(named: "Chevron-Dn-Wht")
+        theImageView.tag = kHeaderSectionTag + section
+        header.addSubview(theImageView)
+        
+        // make headers touchable
+        header.tag = section
+        let headerTapGesture = UITapGestureRecognizer()
+        headerTapGesture.addTarget(self, action: #selector(MenuItemsVC.sectionHeaderWasTouched(_:)))
+        header.addGestureRecognizer(headerTapGesture)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Expand / Collapse Methods
+    
+    @objc func sectionHeaderWasTouched(_ sender: UITapGestureRecognizer) {
+        let headerView = sender.view as! UITableViewHeaderFooterView
+        let section    = headerView.tag
+        let eImageView = headerView.viewWithTag(kHeaderSectionTag + section) as? UIImageView
+        
+        if (self.expandedSectionHeaderNumber == -1) {
+            self.expandedSectionHeaderNumber = section
+            tableViewExpandSection(section, imageView: eImageView!)
+        } else {
+            if (self.expandedSectionHeaderNumber == section) {
+                tableViewCollapeSection(section, imageView: eImageView!)
+            } else {
+                let cImageView = self.view.viewWithTag(kHeaderSectionTag + self.expandedSectionHeaderNumber) as? UIImageView
+                tableViewCollapeSection(self.expandedSectionHeaderNumber, imageView: cImageView!)
+                tableViewExpandSection(section, imageView: eImageView!)
+            }
+        }
+    }
+    
+    func tableViewCollapeSection(_ section: Int, imageView: UIImageView) {
+        let sectionData = self.sectionItems[section] as! NSArray
+        
+        self.expandedSectionHeaderNumber = -1;
+        if (sectionData.count == 0) {
+            return;
+        } else {
+            UIView.animate(withDuration: 0.4, animations: {
+                imageView.transform = CGAffineTransform(rotationAngle: (0.0 * CGFloat(Double.pi)) / 180.0)
+            })
+            var indexesPath = [IndexPath]()
+            for i in 0 ..< sectionData.count {
+                let index = IndexPath(row: i, section: section)
+                indexesPath.append(index)
+            }
+            self.menuTableView!.beginUpdates()
+            self.menuTableView!.deleteRows(at: indexesPath, with: UITableViewRowAnimation.fade)
+            self.menuTableView!.endUpdates()
+        }
+    }
+    
+    func tableViewExpandSection(_ section: Int, imageView: UIImageView) {
+        let sectionData = self.sectionItems[section] as! NSArray
+        
+        if (sectionData.count == 0) {
+            self.expandedSectionHeaderNumber = -1;
+            return;
+        } else {
+            UIView.animate(withDuration: 0.4, animations: {
+                imageView.transform = CGAffineTransform(rotationAngle: (180.0 * CGFloat(Double.pi)) / 180.0)
+            })
+            var indexesPath = [IndexPath]()
+            for i in 0 ..< sectionData.count {
+                let index = IndexPath(row: i, section: section)
+                indexesPath.append(index)
+            }
+            self.expandedSectionHeaderNumber = section
+            self.menuTableView!.beginUpdates()
+            self.menuTableView!.insertRows(at: indexesPath, with: UITableViewRowAnimation.fade)
+            self.menuTableView!.endUpdates()
+        }
     }
     
     
@@ -93,7 +224,12 @@ extension MenuItemsVC:UITableViewDelegate,UITableViewDataSource{
     
     @available(iOS 2.0, *)
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if (self.expandedSectionHeaderNumber == section) {
+            let arrayOfItems = self.sectionItems[section] as! NSArray
+            return arrayOfItems.count;
+        } else {
+            return 0;
+        }
         
     }
     
